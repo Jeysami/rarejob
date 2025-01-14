12df2cfd60f4cee108b5baa77e70fe5e7310953f
Jeysami <52683066+Jeysami@users.noreply.github.com>
2025-01-14 12:40:35 +0900

    Add files via upload

---
+++ blogpost/includes/top.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="navbar">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="home.php">
+         </a>
+     </div>
+     <!-- navbar-top-links -->
+     <ul class="nav navbar-top-links navbar-right">
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-3x"></i>
+            </a>
+            <!-- dropdown user-->
+            <ul class="dropdown-menu dropdown-user">
+                <li><a href="changepass.php" ><i class="fa fa-gear fa-fw"></i>Settings</a></li>
+                <li class="divider"></li>
+                <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i>Logout</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
+<!-- end navbar top -->
